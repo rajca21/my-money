07693b7f8d641a1fb87131b09e1b2e0777be3cd9
rajca21 <2nikolar1@gmail.com>
2022-06-25 06:13:44 +0200

    firebase update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBvCAjJTJFOO9CxSBMC65BmnsRBMkvNUeo",
-    authDomain: "mymoney-54fc2.firebaseapp.com",
-    projectId: "mymoney-54fc2",
-    storageBucket: "mymoney-54fc2.appspot.com",
-    messagingSenderId: "921064340631",
-    appId: "1:921064340631:web:51830342194c8a99ecfdd2"
-};
+    apiKey: "AIzaSyD3Cqubd88b3GJEmxuXFx-PT3Nq-exopmM",
+    authDomain: "my-money-37f46.firebaseapp.com",
+    projectId: "my-money-37f46",
+    storageBucket: "my-money-37f46.appspot.com",
+    messagingSenderId: "34027595257",
+    appId: "1:34027595257:web:816ef2a37be80f6e9e0928"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
